20bfa5412f68ac0d9ca1931aab1c29f699a36cb5
hoonga <etaphirho@snu.ac.kr>
2016-03-21 15:58:20 +0900

    changed skeleton

--- prj1/main.cpp
+++ prj1/main.cpp
@@ -5,3 +5,17 @@
 #include<algorithm>
 
 #include"node.h"
+
+void resize();
+void display();
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800,600);
+    glutInitWindowPosition(0,0);
+    glutCreateWindow("GLaDOS");
+
+    glutReshapeFunc(resize);
+    glutDisplayFunc(display);
+}

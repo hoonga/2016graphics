e41c2cbc3b13d8238757c0def81e1099b5d1e42e
hoonga <etaphirho@snu.ac.kr>
2016-03-21 16:15:07 +0900

    transformation implemented

---
+++ prj1/transformation.cpp
@@ -0,0 +1,8 @@
+#include"transformation.h"
+
+Transformation::Transformation(Node &parent, Node &child, GLfloat* matrix)
+    : parent(parent), child(child)
+{
+    for (int i; i < 16; i++)
+        this->matrix[i] = matrix[i];
+}
--- prj1/transformation.h
+++ prj1/transformation.h
@@ -8,7 +8,7 @@ class Transformation {
         // Translation must have both parent and child
         Transformation(Node &parent, Node &child, GLfloat* matrix);
 
-        GLfloat matirx[16];
+        GLfloat matrix[16];
         Node& parent;
         Node& child;
 };
